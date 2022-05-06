96c501a6da5c908778d1a2fdd4bd1cfcad749383
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-05-06 10:48:10 -0700

    adding Nav and About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
